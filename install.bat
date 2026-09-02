16549c74804bd522ea7e3e709fee4acaa2c508aa
RainnXD (batata123muitoboa-dot) <batata123muitoboa@gmail.com>
2026-09-02 16:02:46 -0300

    Add installation script for RScript in Portuguese

---
+++ install.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo ONLY AVAILABLE IN PORTUGUESE!! -- APENAS DISPONIVEL EM PORTUGUES!!
+timeout /t 1 /nobreak >nul
+
+git clone https://github.com/batata123muitoboa-dot/RScript.git
+
+cd RScript
+
+echo RScript instalado em %cd%. Use com python rscpt.py seu-arquivo.rscpt
+
+pause
