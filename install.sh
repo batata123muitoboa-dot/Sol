47d4d02f35e2de505669284a5c8ba62196d161c4
RainnXD (batata123muitoboa-dot) <batata123muitoboa@gmail.com>
2026-09-02 15:57:13 -0300

    Enhance install.sh with usage message
    Updated install script to include usage instructions in Portuguese.

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "ONLY AVAILABLE IN PORTUGUESE!! -- APENAS DISPONÍVEL EM PORTUGUÊS!!"
+sleep 1
+
+git clone https://github.com/batata123muitoboa-dot/RScript.git
+cd RScript
+
+echo "RScript instalado em $(pwd). Use com python rscpt.py seu-arquivo.rscpt"
