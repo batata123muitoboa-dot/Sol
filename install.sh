293efe3c7fcd3a951bdfe10ec618154a7520ca9c
RainnXD (batata123muitoboa-dot) <batata123muitoboa@gmail.com>
2026-09-06 16:38:31 -0300

    Update echo message for RScript usage

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ sleep 1
 git clone https://github.com/batata123muitoboa-dot/RScript.git
 cd RScript
 
-echo "RScript instalado em $(pwd). Use com python rscpt.py seu-arquivo.rscpt"
+echo "RScript instalado em $(pwd). Use com python rscpl.py seu-arquivo.rscpl"
