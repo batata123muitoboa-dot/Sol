51dabd458fa135da44214328c2fbe73fce815309
RainnXD (batata123muitoboa-dot) <batata123muitoboa@gmail.com>
2026-09-07 15:02:48 -0300

    Update installation message and script name in install.bat

--- install.bat
+++ install.bat
@@ -7,6 +7,6 @@ git clone https://github.com/batata123muitoboa-dot/RScript.git
 
 cd RScript
 
-echo RScript instalado em %cd%. Use com python rscpl.py seu-arquivo.rscpl
+echo RScript instalado em %cd%. Use com python soll.py seu-arquivo.soll
 
 pause
